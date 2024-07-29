0d86996a1efdcc9b7d9509e071343125d3d217e5
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-29 17:06:19 -0400

    Update form with DatePicker

--- frontend/src/CreateNewTripPage.js
+++ frontend/src/CreateNewTripPage.js
@@ -1,11 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import Button from "react-bootstrap/Button"
 import Col from "react-bootstrap/Col"
 import Form from "react-bootstrap/Form"
 import Row from "react-bootstrap/Row"
+import DatePicker from "react-datepicker"
+import "react-datepicker/dist/react-datepicker.css"
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 function CreateNewTripPage({ locations }) {
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+
   return (
     <div className="form-container">
       <Form>
@@ -47,28 +52,23 @@ function CreateNewTripPage({ locations }) {
           <Form.Control placeholder="Camping Trip to Utah" />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formGridAddress2">
-          <Form.Label>Address 2</Form.Label>
-          <Form.Control placeholder="Apartment, studio, or floor" />
-        </Form.Group>
-
         <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>Country</Form.Label>
-            <Form.Control />
+          <Form.Group as={Col} controlId="formGridStartDate">
+            <Form.Label>Start Date</Form.Label>
+            <DatePicker
+              selected={startDate}
+              onChange={(startDate) => setStartDate(startDate)}
+            />
           </Form.Group>
 
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State/Province</Form.Label>
-            <Form.Control />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
+          <Form.Group as={Col} controlId="formGridEndDate">
+            <Form.Label>End Date</Form.Label>
+            <DatePicker
+              selected={endDate}
+              onChange={(endDate) => setEndDate(endDate)}
+            />{" "}
           </Form.Group>
         </Row>
-
         <Button variant="primary" type="submit">
           Submit
         </Button>
