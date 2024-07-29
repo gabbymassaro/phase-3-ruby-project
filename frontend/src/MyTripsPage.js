56df2a3ca66b4acdec408e96d6d0165009b2bb35
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-29 14:05:39 -0400

    Fix DOM error for table

--- frontend/src/MyTripsPage.js
+++ frontend/src/MyTripsPage.js
@@ -7,24 +7,28 @@ function MyTripsPage({ trips }) {
   return (
     <div className="tripstable">
       <table>
-        <tr>
-          <th>Title</th>
-          <th>Start Date</th>
-          <th>End Date</th>
-          <th>Locations</th>
-          <th>Activities</th>
-          <th>Lodging</th>
-        </tr>
-        {trips.map((trip, index) => (
-          <tr key={index}>
-            <td>{trip.title}</td>
-            <td>{trip.start_date}</td>
-            <td>{trip.end_date}</td>
-            <td>{trip.location_id}</td>
-            <td>{trip.activity_id}</td>
-            <td>{trip.lodging_id}</td>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Start Date</th>
+            <th>End Date</th>
+            <th>Locations</th>
+            <th>Activities</th>
+            <th>Lodging</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {trips.map((trip, index) => (
+            <tr key={index}>
+              <td>{trip.title}</td>
+              <td>{trip.start_date}</td>
+              <td>{trip.end_date}</td>
+              <td>{trip.location_id}</td>
+              <td>{trip.activity_id}</td>
+              <td>{trip.lodging_id}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   )
