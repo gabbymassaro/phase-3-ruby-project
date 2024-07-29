2563631604a739115a918e04b4bf8436d7a943d0
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-29 14:28:15 -0400

    Minor update to trips output

--- frontend/src/MyTripsPage.js
+++ frontend/src/MyTripsPage.js
@@ -2,8 +2,6 @@ import React from "react"
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 function MyTripsPage({ trips }) {
-  // return <div>hi</div>
-
   return (
     <div className="tripstable">
       <table>
@@ -12,8 +10,8 @@ function MyTripsPage({ trips }) {
             <th>Title</th>
             <th>Start Date</th>
             <th>End Date</th>
-            <th>Locations</th>
-            <th>Activities</th>
+            <th>City</th>
+            <th>Activity</th>
             <th>Lodging</th>
           </tr>
         </thead>
@@ -23,9 +21,9 @@ function MyTripsPage({ trips }) {
               <td>{trip.title}</td>
               <td>{trip.start_date}</td>
               <td>{trip.end_date}</td>
-              <td>{trip.location_id}</td>
-              <td>{trip.activity_id}</td>
-              <td>{trip.lodging_id}</td>
+              <td>{trip.location.city}</td>
+              <td>{trip.activity.name}</td>
+              <td>{trip.lodging.name}</td>
             </tr>
           ))}
         </tbody>
