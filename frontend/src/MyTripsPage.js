503f5c84cad02016edb856ccff0ec0259ea5947c
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-29 13:50:56 -0400

    Very basic table setup, displays all trips on 'My Travels'

--- frontend/src/MyTripsPage.js
+++ frontend/src/MyTripsPage.js
@@ -2,7 +2,32 @@ import React from "react"
 import "bootstrap-icons/font/bootstrap-icons.css"
 
 function MyTripsPage({ trips }) {
-  return <div>hi</div>
+  // return <div>hi</div>
+
+  return (
+    <div className="tripstable">
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Start Date</th>
+          <th>End Date</th>
+          <th>Locations</th>
+          <th>Activities</th>
+          <th>Lodging</th>
+        </tr>
+        {trips.map((trip, index) => (
+          <tr key={index}>
+            <td>{trip.title}</td>
+            <td>{trip.start_date}</td>
+            <td>{trip.end_date}</td>
+            <td>{trip.location_id}</td>
+            <td>{trip.activity_id}</td>
+            <td>{trip.lodging_id}</td>
+          </tr>
+        ))}
+      </table>
+    </div>
+  )
 }
 
 export default MyTripsPage
