7486304d26bb6201cfc3a88a36b82ec05a0a6802
Gabriella Massaro <gmassaro807@gmail.com>
2024-07-30 13:56:49 -0400

    Pull form into it's own component

--- frontend/src/MyTripsPage.js
+++ frontend/src/MyTripsPage.js
@@ -1,69 +1,58 @@
 import React from "react"
 import Table from "react-bootstrap/Table"
 import "bootstrap-icons/font/bootstrap-icons.css"
-
 function MyTripsPage({ trips }) {
-  const handleOnClick = (id) => {
-    console.log(id)
-    fetch(`http://localhost:9292/trips/${id}`)
-      .then((r) => r.json())
-      .then((trips) => console.log(trips))
-  }
-
   return (
-    <div className="tripstable">
-      <Table striped bordered hover size="sm">
-        <thead>
-          <tr>
-            <th>Trip</th>
-            <th>Start Date</th>
-            <th>End Date</th>
-            <th>City</th>
-            <th>Activities</th>
-            <th>Lodging</th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          {trips.map((trip, index) => (
-            <tr key={index}>
-              <td>{trip.title}</td>
-              <td>{trip.start_date}</td>
-              <td>{trip.end_date}</td>
-              <td>{trip.location.city}</td>
-              <td>
-                {trip.activities.map((activity, idx) => (
-                  <span key={idx}>
-                    {activity.name}
-                    {idx < trip.activities.length - 1 ? ", " : ""}
-                  </span>
-                ))}
-              </td>
-              <td>
-                {trip.lodgings.map((lodging, idx) => (
-                  <span key={idx}>
-                    {lodging.name}
-                    {idx < trip.lodgings.length - 1 ? ", " : ""}
-                  </span>
-                ))}
-              </td>
-              <td>
-                <button
-                  className="btn btn-primary"
-                  onClick={() => handleOnClick(trip.id)}
-                >
-                  Update
-                </button>
-              </td>
-              <td>
-                <button className="btn btn-danger">Remove</button>
-              </td>
+    <>
+      <div className="tripstable">
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Trip</th>
+              <th>Start Date</th>
+              <th>End Date</th>
+              <th>City</th>
+              <th>Activities</th>
+              <th>Lodging</th>
+              <th></th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-    </div>
+          </thead>
+          <tbody>
+            {trips.map((trip, index) => (
+              <tr key={index}>
+                <td>{trip.title}</td>
+                <td>{trip.start_date}</td>
+                <td>{trip.end_date}</td>
+                <td>{trip.location.city}</td>
+                <td>
+                  {trip.activities.map((activity, idx) => (
+                    <span key={idx}>
+                      {activity.name}
+                      {idx < trip.activities.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
+                </td>
+                <td>
+                  {trip.lodgings.map((lodging, idx) => (
+                    <span key={idx}>
+                      {lodging.name}
+                      {idx < trip.lodgings.length - 1 ? ", " : ""}
+                    </span>
+                  ))}
+                </td>
+                <td>
+                  <button className="btn btn-primary">Update</button>
+                </td>
+                <td>
+                  <button className="btn btn-danger">Remove</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    </>
   )
 }
 
